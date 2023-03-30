6ec328cc0ba418fad09a1a0b227475db633da123
mercymugambi <mercymugambi2016@gmail.com>
2023-03-31 01:36:18 +0300

    Fix local storage and indexing of items

--- src/index.js
+++ src/index.js
@@ -5,44 +5,58 @@ import AddItems from './print';
 import './style.css';
 
 const TodoContainer = document.querySelector('.list-items');
+// Check if To Do List exists in local storage
+const storedItems = localStorage.getItem('items');
 
 // task/object array declared empty
-const TodoList = [];
+let TodoList = [];
+
+// Load To Do List from local storage if it exists
+if (storedItems) {
+  TodoList = JSON.parse(storedItems);
+}
+
+// Render To Do List
+AddItems(TodoList, TodoContainer);
 
 // Event listener to add tasks to the to do list when enter is pressed.
 const inputField = document.querySelector('.addItem');
 inputField.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
-    const newItem = { description: inputField.value };
+    const newItem = {
+      description: inputField.value,
+      completed: false,
+      index: TodoList.length,
+    };
     TodoList.push(newItem);
+    // Save the updated To Do List in local storage
+    localStorage.setItem('items', JSON.stringify(TodoList));
     AddItems(TodoList, TodoContainer);
     inputField.value = '';
   }
 });
 
-// EventListener to add items to the list when the drop down icon is clicked
+const todoContainer = document.querySelector('.list-items');
 
-const icon = document.querySelector('#icon');
-icon.addEventListener('click', () => {
-  const newItem = { description: inputField.value };
-  TodoList.push(newItem);
-  AddItems(TodoList, TodoContainer);
-  inputField.value = '';
-});
+todoContainer.addEventListener('click', (event) => {
+  const { target } = event;
 
-// Get all the li elements
-const liElements = Element.querySelectorAll('li');
+  if (target.tagName === 'P') {
+    target.setAttribute('contenteditable', true);
 
-// Add a change event listener to each li element
-liElements.forEach((li) => {
-  const checkbox = li.querySelector('.Checkboxi');
-  checkbox.addEventListener('change', () => {
-    // Get the index of the task associated with the li element
-    const index = parseInt(li.dataset.index, 10);
-    // If the checkbox is checked, remove the task from the TodoList array and re-render the list
-    if (checkbox.checked) {
-      TodoList.splice(index, 1);
-      AddItems(TodoList, TodoContainer);
-    }
-  });
+    target.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        target.removeAttribute('contenteditable');
+        const index = parseInt(target.parentElement.dataset.index, 10);
+        const newDescription = target.textContent.trim();
+
+        // Update the task description in the array
+        if (Array.isArray(TodoList) && index >= 0 && index < TodoList.length) {
+          TodoList[index].description = newDescription;
+          // Save the updated To Do List in local storage
+          localStorage.setItem('items', JSON.stringify(TodoList));
+        }
+      }
+    });
+  }
 });
--- src/print.js
+++ src/print.js
@@ -2,18 +2,32 @@ const AddItems = (Items, Element) => {
   let finalHtmlItem = '';
   Items.forEach((todo, index) => {
     finalHtmlItem += `
-      <li class = "li-list" data-index="${index}">
+      <li class = "li-list" data-index="${index + 1}">
       <div class="listdiv">
         <input type="checkbox" class="Checkboxi" />
         <p>${todo.description}</p>
       </div>
-      <i class="fa-solid fa-ellipsis-vertical icon-remove"></i>
+      <i class="fa-solid fa-ellipsis-vertical remove-icon"></i>
       </li>
       <div class="borderLine"></div>
     `;
   });
 
   Element.innerHTML = finalHtmlItem;
+
+  // Add event listener to each remove icon
+  const removeIcons = document.querySelectorAll('.remove-icon');
+  removeIcons.forEach((icon) => {
+    icon.addEventListener('click', (event) => {
+      const li = event.target.closest('li');
+      const index = parseInt(li.dataset.index, 10); // Specify radix 10 to avoid warnings
+      const checkbox = li.querySelector('.Checkboxi');
+      if (checkbox.checked) {
+        Items.splice(index - 1, 1); // Remove the corresponding element from the array
+        AddItems(Items, Element); // Re-render the list
+      }
+    });
+  });
 };
 
 export default AddItems;
